f5fc2ac6502160fbd5359a9b241c4bea8e13e7a9
Bruno PM <brunoporras2001@gmail.com>
2024-09-30 18:06:12 -0500

    Adding schemas to idiomas module

--- src/drizzle/schemas/usuario.ts
+++ src/drizzle/schemas/usuario.ts
@@ -1,11 +1,22 @@
-import { int, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import { int, mysqlTable, unique, varchar } from "drizzle-orm/mysql-core";
 import { Facultad } from "./facultad";
 
-export const Usuario = mysqlTable("Usuario", {
-  id: int("id").primaryKey().autoincrement(),
-  facultad_id: int("facultad_id").references(() => Facultad.id),
-  email: varchar("email", { length: 50 }).unique(),
-  password: varchar("password", { length: 255 }).notNull(),
-  nickname: varchar("nickname", { length: 50 }).notNull(),
-  modulo: varchar("modulo", { length: 50 }).notNull(),
-});
+export const Usuario = mysqlTable(
+  "Usuario",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    facultad_id: int("facultad_id").references(() => Facultad.id),
+    email: varchar("email", { length: 50 }),
+    password: varchar("password", { length: 255 }).notNull(),
+    nickname: varchar("nickname", { length: 50 }).notNull(),
+    modulo: varchar("modulo", { length: 50 }).notNull(),
+  },
+  (table) => {
+    return {
+      usuarioUnicoPorModulo: unique("usuarioUnicoPorModulo").on(
+        table.email,
+        table.modulo
+      ),
+    };
+  }
+);
